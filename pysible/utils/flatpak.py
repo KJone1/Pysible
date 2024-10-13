8ab4a602d679ec290a2f4151c686cf403fe92b57
KJone1 <davidyahin@protonmail.com>
2024-10-13 21:17:28 +0300

    Use sudo for flatpak remote-add

--- pysible/utils/flatpak.py
+++ pysible/utils/flatpak.py
@@ -1,12 +1,16 @@
-from sh import flatpak
+from sh import flatpak, contrib
 from loguru import logger
+from os import getenv
 
 
 def setup_flatpak_repo():
+    """Add remote repo for flatpak if does not exists"""
     logger.info("Insuring flathub repo exists...")
-    flatpak(
-        "remote-add",
-        "--if-not-exists",
-        "flathub",
-        "https://dl.flathub.org/repo/flathub.flatpakrepo",
-    )
+    root_pass = getenv("ROOT_PASS")
+    with contrib.sudo(password=root_pass, _with=True):
+        flatpak(
+            "remote-add",
+            "--if-not-exists",
+            "flathub",
+            "https://dl.flathub.org/repo/flathub.flatpakrepo",
+        )
