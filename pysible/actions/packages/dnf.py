dce71981fcb9a428d42e9f7ed0a98b2a71057771
KJone1 <davidyahin@protonmail.com>
2024-10-13 21:11:09 +0300

    Split packages to flatpak.py and dnf.py

---
+++ pysible/actions/packages/dnf.py
@@ -0,0 +1,61 @@
+from concurrent.futures import ThreadPoolExecutor
+from pysible.utils.install_package import install_package
+from loguru import logger
+from tqdm import tqdm
+
+
+def install_dnf():
+    """
+    Installs a list of packages in parallel using dnf.
+    """
+
+    package_list = {
+        "code",
+        "helm",
+        "vlc",
+        "solaar",
+        "neovim",
+        "qbittorrent",
+        "wireguard-tools",
+        "thunar",
+        "sway",
+        "waybar",
+        "wl-clipboard",
+        "grimshot",
+        "fd-find",
+        "lazygit",
+        "ripgrep",
+        "fzf",
+        "bat",
+        "stow",
+        "NetworkManager-tui",
+        "wofi",
+        "go-task",
+        "wlogout",
+        "cowsay",
+        "flatpak",
+        "zsh",
+    }
+    total_packages = len(package_list)
+    installed_packages = 0
+
+    logger.info(f"Starting installation of {total_packages} dnf packages.")
+
+    with tqdm(total=total_packages, desc="Installing...") as pbar:
+
+        def install_dnf_packages(package_name):
+            # ThreadPoolExecutor() does not take in arguments for the function
+            # so doing this hack so i can pass "dnf" as package manager
+            nonlocal installed_packages
+            status = install_package(package=package_name, package_manager="dnf")
+            if status:
+                installed_packages += 1
+            pbar.update(1)
+            return status
+
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            executor.map(install_dnf_packages, package_list)
+
+    logger.info(
+        f"{installed_packages}/{total_packages} dnf packages installed successfully."
+    )
---
+++ pysible/actions/packages/flatpak.py
@@ -0,0 +1,45 @@
+from concurrent.futures import ThreadPoolExecutor
+from pysible.utils.install_package import install_package
+from loguru import logger
+from pysible.utils.flatpak import setup_flatpak_repo
+
+from tqdm import tqdm
+
+
+def install_flatpak():
+    """
+    Installs a list of packages in parallel using flatpak.
+    """
+    setup_flatpak_repo()
+
+    package_list = {
+        "com.discordapp.Discord",
+        "io.github.flattool.Warehouse",
+        "net.nokyan.Resources",
+        "md.obsidian.Obsidian",
+        "io.github.zen_browser.zen",
+        "com.github.tchx84.Flatseal",
+    }
+    total_packages = len(package_list)
+    installed_packages = 0
+
+    logger.info(f"Starting installation of {total_packages} flatpak packages.")
+
+    with tqdm(total=total_packages, desc="Installing...") as pbar:
+
+        def install_flatpak_packages(package_name):
+            # ThreadPoolExecutor() does not take in arguments for the function
+            # so doing this hack so i can pass "dnf" as package manager
+            nonlocal installed_packages
+            status = install_package(package=package_name, package_manager="flatpak")
+            if status:
+                installed_packages += 1
+            pbar.update(1)
+            return status
+
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            executor.map(install_flatpak_packages, package_list)
+
+    logger.info(
+        f"{installed_packages}/{total_packages} flatpak packages installed successfully."
+    )
