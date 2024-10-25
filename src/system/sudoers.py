417f21800d5aa87740995e7dbdc50791ae75edbb
KJone1 <davidyahin@protonmail.com>
2024-10-25 23:05:47 +0300

    Use RESOURCE_DIR from const file

--- src/system/sudoers.py
+++ src/system/sudoers.py
@@ -3,12 +3,14 @@ from loguru import logger
 
 from sh import visudo, ErrorReturnCode
 
+import src.config.constants as const
+
 
 def setup_sudoers_for_user() -> None:
-    SUDOERS_FILE_NAME = "kj"
+    SUDOERS_FILE_NAME = f"{const.RESOURCES_DIR}/kj"
     SUDOERS_PATH = "/etc/sudoers.d/kj"
     try:
-        visudo("-c", "-f", f"resources/{SUDOERS_FILE_NAME}")
+        visudo("-c", "-f", SUDOERS_FILE_NAME)
         copy_resource(filename=SUDOERS_FILE_NAME, dest=SUDOERS_PATH, sudo=True)
     except ErrorReturnCode as e:
         logger.error(f"Error validating sudoers file: {e}")
