45f23f12f2fd3abf4582aab484eafbe31f642b53
KJone1 <davidyahin@protonmail.com>
2024-10-25 12:13:27 +0300

    Add sudo_run function

--- src/system/sudoers.py
+++ src/system/sudoers.py
@@ -9,7 +9,7 @@ def setup_sudoers_for_user() -> None:
     SUDOERS_PATH = "/etc/sudoers.d/kj"
     try:
         visudo("-c", "-f", f"resources/{SUDOERS_FILE_NAME}")
-        copy_resource(filename=SUDOERS_FILE_NAME, dest=SUDOERS_PATH)
+        copy_resource(filename=SUDOERS_FILE_NAME, dest=SUDOERS_PATH, sudo=True)
     except ErrorReturnCode as e:
         logger.error(f"Error validating sudoers file: {e}")
     except FileNotFoundError as e:
--- src/utils/misc_utils.py
+++ src/utils/misc_utils.py
@@ -1,6 +1,5 @@
-from sh import mkdir
-
-from os import path
+import sh
+from os import path, getenv
 from shutil import rmtree
 
 
@@ -8,9 +7,25 @@ ROOT_TMP_DIR = "tmp"
 
 
 def create_tmp_dir(name: str) -> None:
-    mkdir("-p", f"{ROOT_TMP_DIR}/{name}")
+    sh.mkdir("-p", f"{ROOT_TMP_DIR}/{name}")
 
 
 def delete_tmp_dir() -> None:
     if path.exists(ROOT_TMP_DIR):
         rmtree(ROOT_TMP_DIR)
+
+
+def sudo_run(command: str, *args) -> None:
+    """
+    executes a command with sudo privileges.
+
+    args:
+        command: the command to execute (e.g., 'make', 'ls', 'cp').
+        *args:  any arguments to pass to the command.
+
+    raises:
+        sh.ErrorReturnCode: if the command execution fails.
+    """
+    root_pass = getenv("root_pass")
+    with sh.contrib.sudo(_with=True, password=root_pass):
+        getattr(sh, command)(*args)
