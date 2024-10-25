49ca6878ebb5c5dec25ac469a79c26a495030014
KJone1 <davidyahin@protonmail.com>
2024-10-25 14:04:00 +0300

    Delete useless imports

--- src/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .install_package import install_package
-from .net import wget, git_clone
-from .temp_dir import create_tmp_dir, delete_tmp_dir
-from .untar import untar
-from .copy import copy_resource
-from .flatpak import setup_flatpak_repo
