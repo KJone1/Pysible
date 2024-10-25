11342357cefef3b00c96362d8faf0a740373e036
KJone1 <davidyahin@protonmail.com>
2024-10-25 22:59:14 +0300

    Add dotfiles tp setup_system()

--- src/system/__init__.py
+++ src/system/__init__.py
@@ -1,9 +1,14 @@
 from .sudoers import setup_sudoers_for_user
 from .sddm import clone_sddm_theme, update_sddm_theme
+from .dotfiles import clone_dotfiles, run_dotfiles_intall_script
 
 
 def setup_system():
     """Sets up system configuration"""
     setup_sudoers_for_user()
-    clone_sddm_theme()
-    update_sddm_theme()
+    clone_ok = clone_sddm_theme()
+    if clone_ok:
+        update_sddm_theme()
+    clone_ok = clone_dotfiles()
+    if clone_ok:
+        run_dotfiles_intall_script()
