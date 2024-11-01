ff36b6b04c3285e674a5c853a7534cee8682b344
KJone1 <davidyahin@protonmail.com>
2024-11-01 20:29:33 +0200

    Init install_kubectl function

--- src/software/containers.py
+++ src/software/containers.py
@@ -1,18 +1,51 @@
-def install_kubectl():
+from loguru import logger
+
+import src.utils.file_utils as files
+
+import src.utils.net_utils as net
+
+
+def install_kubectl() -> None:
+    KUBE_VER = _get_kubernetes_version()
+    KUBECTL_URL = f"https://dl.k8s.io/release/{KUBE_VER}/bin/linux/amd64/kubectl"
+    KUBECTL_DEST = "/usr/local/bin/kubectl"
+
+    try:
+        net.wget(url=KUBECTL_URL, dest=KUBECTL_DEST)
+    except Exception as e:
+        logger.error(f"Failed to download kubectl with error -> {e}")
+    else:
+        files.set_file_permissions(KUBECTL_DEST, "555")
+        logger.into(f"Installed kubectl {KUBE_VER}")
+
+
+def install_nerdctl() -> None:
     pass
 
 
-def install_nerdctl():
+def install_k0s() -> None:
     pass
 
 
-def install_k0s():
+def install_k9s() -> None:
     pass
 
 
-def install_k9s():
+def install_buildkit() -> None:
     pass
 
 
-def install_buildkit():
-    pass
+def _get_kubernetes_version() -> str:
+    """Fetches the latest stable Kubernetes version."""
+    import requests
+
+    url = "https://dl.k8s.io/release/stable.txt"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching Kubernetes version: {e}")
+        return None
+    else:
+        version = response.text.strip()
+        return version
