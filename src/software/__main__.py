974a7bed77c9c9d0bd130c4f8e1ba865ff473d0a
KJone1 <davidyahin@protonmail.com>
2024-11-01 20:29:17 +0200

    Move setup_packages into setup.py

--- src/software/__main__.py
+++ src/software/__main__.py
@@ -1,16 +1,4 @@
-import src.software.containers as containers
-from .dnf import install_dnf
-from .flatpak import install_flatpak
-from .tomb import install_tomb
-
-
-def setup_packages():
-    """Sets up all relevant packages"""
-    # install_dnf()
-    # install_flatpak()
-    # install_tomb()
-    containers.install_kubectl()
-
+import src.software.setup as setup
 
 if __name__ == "__main__":
-    setup_packages()
+    setup.setup_packages()
---
+++ src/software/setup.py
@@ -0,0 +1,12 @@
+import src.software.containers as containers
+from .dnf import install_dnf
+from .flatpak import install_flatpak
+from .tomb import install_tomb
+
+
+def setup_packages():
+    """Sets up all relevant packages"""
+    # install_dnf()
+    # install_flatpak()
+    # install_tomb()
+    containers.install_kubectl()
