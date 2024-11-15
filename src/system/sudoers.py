2b4e0d03832d8c95b5cd536c96e3e92628468d1f
KJone1 <davidyahin@protonmail.com>
2024-11-15 19:55:00 +0200

    Use new Consts class

--- src/system/sudoers.py
+++ src/system/sudoers.py
@@ -1,12 +1,12 @@
 from loguru import logger
 from sh import ErrorReturnCode, visudo
 
-import src.config.constants as const
+from src.config.constants import Consts
 from src.utils.file_utils import copy_resource
 
 
 def setup_sudoers_for_user() -> None:
-    SUDOERS_FILE_NAME = f"{const.RESOURCES_DIR}/kj"
+    SUDOERS_FILE_NAME = f"{Consts.RESOURCES_DIR}/kj"
     SUDOERS_PATH = "/etc/sudoers.d/kj"
     try:
         visudo("-c", "-f", SUDOERS_FILE_NAME)
