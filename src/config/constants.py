27d0cadb3ebc0d160aa0006337ec0eb8c545c039
KJone1 <davidyahin@protonmail.com>
2025-04-15 19:56:43 +0300

    Add default for HOME_DIR const

--- src/config/constants.py
+++ src/config/constants.py
@@ -9,6 +9,6 @@ import src.config.config as config
 class Consts:
     ROOT_PASS: str = config.load_env("ROOT_PASS")
     ROOT_DIR: Path = Path(__file__).absolute().parent.parent.parent
-    HOME_DIR: str = os.getenv("HOME")
+    HOME_DIR: str = os.getenv("HOME", "/home/kj")
     RESOURCES_DIR: Path = ROOT_DIR / "resources"
     TMP_DIR: Path = ROOT_DIR / "tmp"
