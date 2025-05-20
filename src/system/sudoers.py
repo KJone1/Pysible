5b2db7d6c0c4977a5931e1c6a64440b97036961b
KJone1 <davidyahin@protonmail.com>
2025-05-20 12:01:12 +0300

    Move sudoers to playbook file

--- src/system/sudoers.py
+++ src/system/sudoers.py
@@ -5,15 +5,6 @@ from src.config.constants import Consts
 from src.utils.file_utils import copy_resource
 
 
-def setup_sudoers_for_user() -> None:
-    sudoers_file_name = f"{Consts.RESOURCES_DIR}/kj"
-    sudoers_path = "/etc/sudoers.d/kj"
-    try:
-        sh.visudo("-c", "-f", sudoers_file_name)
-        _ = copy_resource(filename=sudoers_file_name, dest=sudoers_path, sudo=True)
-    except sh.ErrorReturnCode as e:
-        logger.error(f"Error validating sudoers file: {e}")
-    except FileNotFoundError as e:
-        logger.error(e)
-    else:
-        logger.info(f"Copied {sudoers_file_name} to {sudoers_path}")
+def setup_sudoers_for_user(sudoers_file_name: str, sudoers_path: str) -> None:
+    sh.visudo("-c", "-f", sudoers_file_name)
+    _ = copy_resource(filename=sudoers_file_name, dest=sudoers_path, sudo=True)
