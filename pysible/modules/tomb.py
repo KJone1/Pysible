d147f659db66dbb68f034c7c32dddb21d4ec3030
KJone1 <davidyahin@protonmail.com>
2025-05-24 08:49:18 +0300

    Refactored tomb module

--- pysible/modules/tomb.py
+++ pysible/modules/tomb.py
@@ -1,10 +1,23 @@
-import pysible.software.tomb as tomb
-from pysible.utils.log_utils import Logger
 import sh
+from pysible.utils.file_utils import untar
+from pysible.utils.misc_utils import create_tmp_dir, sudo_run
+from pysible.utils.net_utils import wget
+from pysible.config.settings import settings
+from pysible.utils.log_utils import Logger
 
 def install_tomb(version:str):
+    tmp_dir_name = "tomb"
+    tmp_dir_path = f"{settings.TMP_DIR}/{tmp_dir_name}"
+    url = f"https://github.com/dyne/tomb/archive/refs/tags/v{version}.tar.gz"
+    file_dest = f"{tmp_dir_path}/{version}.tar.gz"
+
+    Logger.info("Starting to install Tomb...")
     try:
-        tomb.install_tomb(version)
+        create_tmp_dir(name=tmp_dir_name)
+        wget(url=url, dest=file_dest)
+        untar(input=file_dest, output=tmp_dir_path, strip=True)
+        sudo_run("make", "install", _cwd=tmp_dir_path, _out="/dev/null")
+        Logger.success(f"Installed Tomb {version}")
     except sh.ErrorReturnCode as e:
         error = "Encounter an ErrorReturnCode when tried to install Tomb"
         Logger.failure(f"{error} -> {e}")
