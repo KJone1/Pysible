eb2a5a8252f65eab6ff3387b6bbe385e38405118
KJone1 <davidyahin@protonmail.com>
2025-05-26 20:37:34 +0300

    Formatting and migrate to use TaskException

--- pysible/modules/dotfiles.py
+++ pysible/modules/dotfiles.py
@@ -5,25 +5,16 @@ import sh
 import pysible.exceptions.dotfiles as exceptions
 from pysible.config.settings import Sections, settings
 from pysible.core.task_plugin_decorator import task_plugin
+from pysible.exceptions.task_exceptions import TaskFailedException
 from pysible.utils.log_utils import Logger
 from pysible.utils.net_utils import git_clone
-
-
-def _clone_dotfiles(repo_url: str, dest: str) -> None:
-    git_clone(repo_url=repo_url, dest=dest)
-    Logger.info(f"Successfully cloned {repo_url} to {dest}")
 
 
 def _run_dotfiles_install_script(dotfiles_repo_path: str) -> None:
     install_script_path = path.join(dotfiles_repo_path, "install.sh")
     bash = sh.Command("bash")
-    try:
-        _ = bash(install_script_path, _cwd=dotfiles_repo_path)
-        Logger.info("dotfiles installed successfully")
-    except sh.ErrorReturnCode as e:
-        raise exceptions.DotfilesInstallError(
-            "Failed to install dotfiles", original_exception=e
-        ) from e
+    _ = bash(install_script_path, _cwd=dotfiles_repo_path)
+    Logger.info("dotfiles installed successfully")
 
 
 @task_plugin(
@@ -35,15 +26,31 @@ def install_dotfiles():
     repo_url = "https://github.com/KJone1/dotfiles.git"
 
     try:
-        _clone_dotfiles(repo_url=repo_url, dest=dotfile_dir)
+        git_clone(repo_url=repo_url, dest=dotfile_dir)
         _run_dotfiles_install_script(dotfiles_repo_path=dotfile_dir)
         Logger.success("Successfully setup dotfiles")
 
     except sh.ErrorReturnCode as e:
-        Logger.failure(f"Git clone returned a failure status code -> {e}")
+        raise TaskFailedException(
+            task_name=__name__,
+            original_exception=e,
+            error_msg="Git clone returned a failure status code",
+        )
     except exceptions.DotfilesInstallError as e:
-        Logger.failure(f"Error running dotfiles install script -> {e}")
+        raise TaskFailedException(
+            task_name=__name__,
+            original_exception=e,
+            error_msg="Error running dotfiles install script",
+        )
     except AttributeError as e:
-        Logger.failure(f"Git not found -> {e}")
+        raise TaskFailedException(
+            task_name=__name__,
+            original_exception=e,
+            error_msg="Git not found",
+        )
     except Exception as e:
-        Logger.failure(f"Failed to Setup dotfiles -> {e}")
+        raise TaskFailedException(
+            task_name=__name__,
+            original_exception=e,
+            error_msg="Caught unexpected dotfiles error",
+        )
