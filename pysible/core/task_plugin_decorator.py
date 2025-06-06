1da96a3d9c6e05d24f1f3582afc57129fb6a974e
KJone1 <davidyahin@protonmail.com>
2025-06-06 18:36:24 +0300

    Add type hints for the decorator

--- pysible/core/task_plugin_decorator.py
+++ pysible/core/task_plugin_decorator.py
@@ -1,11 +1,11 @@
-import functools
+from types import FunctionType
 
 from pysible.config.settings import Sections, settings
 from pysible.core.task import Task
 
 
 def task_plugin(name: str, section: Sections, params: dict[str, str] | None = None):
-    def decorator(func):
+    def decorator(func: FunctionType) -> FunctionType:
         current_task_number = len(settings.REGISTERED_TASKS) + 1
         task_instance = Task(
             number=current_task_number,
