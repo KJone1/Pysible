003b850eaff6b64bef027f5321eadce49d83daa7
KJone1 <davidyahin@protonmail.com>
2025-06-06 18:29:22 +0300

    Remove dead code

--- pysible/core/task_plugin_decorator.py
+++ pysible/core/task_plugin_decorator.py
@@ -6,19 +6,15 @@ from pysible.core.task import Task
 
 def task_plugin(name: str, section: Sections, params: dict[str, str] | None = None):
     def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-
         current_task_number = len(settings.REGISTERED_TASKS) + 1
         task_instance = Task(
             number=current_task_number,
             name=name,
-            action_function=wrapper,
+            action_function=func,
             section=section.value,
             params=params if params else {},
         )
         settings.REGISTERED_TASKS.append(task_instance)
-        return wrapper
+        return func
 
     return decorator
